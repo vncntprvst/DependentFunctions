abb50bace05cd9dffda48854d08c5372634c47fc
Vincent Prevosto <vincent.prevosto@duke.edu>
2019-05-02 15:52:02 -0400

    adds delimiter and header options

--- ImportCSVasVector.m
+++ ImportCSVasVector.m
@@ -1,18 +1,23 @@
-function data=ImportCSVasVector(filename)
-% Import data from csv file. Default delimiter here is space, and assuming
-% floating numbers
-delimiter = ' ';
-
+function data=ImportCSVasVector(filename,delimiter,hasHeader)
+% Import data from csv file. 
+% Sssuming floating numbers
+if nargin==1
+    delimiter = ' '; %Default delimiter is space 
+end
 %first get number of items per line, to define format
 fileID = fopen(filename,'r');
-itemNum=length(regexp(fgets(fileID),' '));
-frewind(fileID);
+itemNum=length(regexp(fgets(fileID),delimiter));
+if ~exist('hasHeader','var') | ~hasHeader
+    frewind(fileID);
+end
 
 % Format for each line of text:
 formatSpec = [repmat('%f',1,itemNum) '%[^\n\r]'];
 
 % Read columns of data according to the format.
-dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'MultipleDelimsAsOne', true, 'TextType', 'string', 'EmptyValue', NaN,  'ReturnOnError', false);
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter,...
+    'MultipleDelimsAsOne', true, 'TextType', 'string',...
+    'EmptyValue', NaN,  'ReturnOnError', false);
 fclose(fileID);
 
 % Allocate imported array to column variable names
